319ca8dfadafc623a5ecaab43a93be3d988fc42a
James Pearce <github@tripleodeon.com>
2024-08-25 14:38:12 -0500

    Restore species table

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,14 @@ export const App = () => {
     // Create the TinyBase Store and initialize the Store's data
     return createStore()
       .setValue('counter', 0)
-      .setRow('pets', '0', { name: 'fido', species: 'dog' });
+      .setRow('pets', '0', { name: 'fido', species: 'dog' })
+      .setTable('species', {
+        dog: { price: 5 },
+        cat: { price: 4 },
+        fish: { price: 2 },
+        worm: { price: 1 },
+        parrot: { price: 3 },
+      });
   });
 
   useCreatePersister(store, async (store) => {
